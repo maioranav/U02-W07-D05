bcf7f4b0ac1bbe4ff6a7bcbc9a300ad28699423d
Vincenzo M <vincenzomaiorana@gmail.com>
2023-01-27 11:39:03 +0100

    add favourite song to DOM

--- assets/index.js
+++ assets/index.js
@@ -9,7 +9,7 @@ const fetchSongs = async function (url, storagePosition, many = 1) {
          let data = await response.json()
          if (many === 1) {
             queryResults = data.data[0]
-            // favSong(queryResults)
+            favSong(queryResults)
             sessionStorage.setItem(storagePosition, JSON.stringify(queryResults))
          } else if (typeof many === "number") {
             queryResults = []
@@ -50,6 +50,27 @@ const list4songs = (array) => {
    }
 }
 
+const favSong = (object) => {
+   let libraryDOM = document.querySelector("#secondSection .favSong")
+   console.log(object)
+   libraryDOM.innerHTML = `
+            <img class="rounded border border-1"
+               src="${object.album.cover_big}"
+               alt="Card image" width="300px" height="300px">
+            <div class="card-body p-5 text-center text-lg-start">
+               <h4 class="card-title display-5">${object.title_short}</h4>
+               <h5 class="card-title my-5">
+               <img src="${object.artist.picture_small}" width="50px" height="50px">
+               ${object.artist.name}</h5>
+               <p class="card-text my-4 fst-italic">da: ${object.album.title}</p>
+               <audio controls class="rounded bg-primary">
+                  <source src="${object.preview}"
+                     type="audio/mpeg">
+               </audio>
+            </div>
+                  `;
+
+}
 
 window.onload = fetchSongs(songUrl, "favSong", 1)
 window.onload = fetchSongs(artistUrl, "artist", 4)
